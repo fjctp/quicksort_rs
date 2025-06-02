629274678aea2af3ddc4801a43110fd590750031
fjctp <snchan20@yahoo.com>
2025-06-01 22:36:03 -0700

    feat: use PartialOrd instead of Ord to support f64.

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,7 @@
 /// quicksort(&mut data);
 /// assert_eq!(data, vec![1, 1, 3, 4, 5, 9]);
 /// ```
-pub fn quicksort<T: Ord>(arr: &mut [T]) {
+pub fn quicksort<T: PartialOrd>(arr: &mut [T]) {
     // Base case: a slice with 0 or 1 elements is already sorted
     if arr.len() <= 1 {
         return;
@@ -62,7 +62,7 @@ pub fn quicksort<T: Ord>(arr: &mut [T]) {
 /// # Panics
 ///
 /// - If the input slice is empty. This function assumes `arr.len() >= 1`.
-fn partition<T: Ord>(arr: &mut [T]) -> usize {
+fn partition<T: PartialOrd>(arr: &mut [T]) -> usize {
     let len = arr.len();
     let mut i = 0; // i marks the boundary of elements <= pivot
 
@@ -87,7 +87,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn test_i64() {
         // Create test data
         let unsorted = vec![33, 10, 55, 71, 29, 3, 18];
 
@@ -104,4 +104,18 @@ mod tests {
         println!("Sorted:   {:?}", uut);
         assert_eq!(uut, exp);
     }
+
+    #[test]
+    fn test_f64() {
+        let unsorted = vec![3.14, -2.0, 0.0, 1.618, 2.71];
+        
+        let mut uut = unsorted.clone();
+        quicksort(&mut uut);
+
+        let mut expected = unsorted.clone();
+        expected.sort_by(|a, b| a.partial_cmp(b).unwrap()); // ensures safe comparison
+
+        assert_eq!(uut, expected);
+    }
+
 }
